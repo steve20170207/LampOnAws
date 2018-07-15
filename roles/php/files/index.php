ce92e1b30aa140d5f551e412fb60d65af1dfee65
steve20170207 <steve.li@ehealthontario.onca>
2018-07-15 19:50:18 -0400

    add playbooks and roles

---
+++ roles/php/files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to DevOps on AWS</title>
+</head>
+<body>
+	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+	<h2><?php echo "This change will be reflected using Ansible pull" ?></h2>
+</body>
+</html>
