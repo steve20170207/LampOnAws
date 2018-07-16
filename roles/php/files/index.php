e0653390c36ca8cf69f6235aa8f8bd20f91cb925
steve20170207 <steve.li@ehealthontario.onca>
2018-07-15 22:17:17 -0400

    add FIFA info to indx.php

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -6,7 +6,7 @@
 <body>
 	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
 	<h2><?php echo "This change will be reflected using Ansible pull" ?></h2>
-        <h3><?php echo "if you see this that mean the absible gain apull works !"      ?></h3>
+        <h3><?php echo "iwelcome FIFA 2018!"      ?></h3>
         <?php phpinfo() ?>
 </body>
 </html>
