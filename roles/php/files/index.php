223df5b724ffc69f18117849b3e170a7ede9f03b
steve20170207 <steve.li@ehealthontario.onca>
2018-07-15 23:05:07 -0400

    ggggggg

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,7 +4,7 @@
 	<title>Welcome to DevOps on AWS</title>
 </head>
 <body>
-	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+	<h1><?php echo "Welcome to DevOps on AWS.this is test page by jiang li " ?></h1>
 	<h2><?php echo "This change will be reflected using Ansible pull" ?></h2>
         <h3><?php echo "iwelcome FIFA 2018!"      ?></h3>
         <?php phpinfo() ?>
